3d186843ad148dfd6ae2c34a48de59ecdd948c8a
Ronald <rdominguez2475@gmail.com>
2022-05-20 12:33:06 -0300

    segundo crud, edicion con parentesis

--- voluntariado/urls.py
+++ voluntariado/urls.py
@@ -3,12 +3,18 @@ from . import views
 
 
 urlpatterns = [
+    path("voluntarios/", views.voluntarios, name="Voluntarios"),
     path("nuevo_voluntario/", views.nuevo_voluntario, name="NuevoVoluntario"),
+    path("voluntario/<id>", views.ver_voluntario, name="VerVoluntario"),
+    path("voluntario/editar/<id>", views.editar_voluntario, name="EditarVoluntario"),  
+    path("voluntario/eliminar/<id>", views.eliminar_voluntario, name="EliminarVoluntario"),
+
     path("facilitadores/", views.facilitadores, name="Facilitadores"),
     path("nuevo_facilitador/", views.nuevo_facilitador, name="NuevoFacilitador"),
     path("facilitador/<id>", views.ver_facilitador, name="VerFacilitador"),
     path("facilitador/editar/<id>", views.editar_facilitador, name="EditarFacilitador"),  
-    path("facilitador/eliminar/<id>", views.eliminar_facilitador, name="EliminarFacilitador"),    
+    path("facilitador/eliminar/<id>", views.eliminar_facilitador, name="EliminarFacilitador"),
+
     path('buscar/<nombre>/', views.buscar_voluntario, name="BuscarVoluntario"),
     path('buscar/resultados', views.resultados_vol, name="BuscarResultados"),
 ]
--- voluntariado/views.py
+++ voluntariado/views.py
@@ -14,31 +14,31 @@ def facilitadores(request):
 
 def ver_facilitador(request, id):
     facilitador = Facilitador.objects.get(id=id)
-    return render(request, "voluntariado/ver_facilitador.html", {"facilitador":facilitador})
+    return render(request, "voluntariado/ver_facilitador.html", {"facilitador": facilitador})
 
 
 def editar_facilitador(request, id):
-    facilitador=Facilitador.objects.get(id=id)
+    facilitador = Facilitador.objects.get(id=id)
 
     if request.method == "POST":
         mi_form = FormFacilitador(request.POST)
         if mi_form.is_valid():
             info = mi_form.cleaned_data
 
-            facilitador.nombre=info["nombre"],
-            facilitador.apellido=info["apellido"],
-            facilitador.email=info["email"],
-            facilitador.presentacion=info["presentacion"],
-            
+            facilitador.nombre = info["nombre"],
+            facilitador.apellido = info["apellido"],
+            facilitador.email = info["email"],
+            facilitador.presentacion = info["presentacion"],
+
             facilitador.save()
             return redirect("Facilitadores")
 
     mi_form = FormFacilitador(initial={
-        "nombre":facilitador.nombre,
-        "apellido":facilitador.apellido,
-        "email":facilitador.email,
-        "presentacion":facilitador.presentacion,
-        })
+        "nombre": facilitador.nombre,
+        "apellido": facilitador.apellido,
+        "email": facilitador.email,
+        "presentacion": facilitador.presentacion,
+    })
 
     return render(request, "voluntariado/formFac.html", {"form": mi_form})
 
@@ -63,12 +63,46 @@ def nuevo_facilitador(request):
 
 
 def eliminar_facilitador(request, id):
-    facilitador=Facilitador.objects.get(id=id)
+    facilitador = Facilitador.objects.get(id=id)
     facilitador.delete()
-        
+
     return redirect("Facilitadores")
 
-    
+
+def voluntarios(request):
+    voluntarios = Voluntario.objects.all()
+    return render(request, "voluntariado/voluntarios.html", {"voluntarios": voluntarios})
+
+
+def ver_voluntario(request, id):
+    voluntario = Voluntario.objects.get(id=id)
+    return render(request, "voluntariado/ver_voluntario.html", {"voluntario": voluntario})
+
+
+def editar_voluntario(request, id):
+    voluntario = Voluntario.objects.get(id=id)
+
+    if request.method == "POST":
+        mi_form = FormVoluntario(request.POST)
+        if mi_form.is_valid():
+            info = mi_form.cleaned_data
+
+            voluntario.nombre = info["nombre"],
+            voluntario.apellido = info["apellido"],
+            voluntario.email = info["email"],
+
+            voluntario.save()
+            return redirect("Voluntarios")
+
+    mi_form = FormVoluntario(initial={
+        "nombre": voluntario.nombre,
+        "apellido": voluntario.apellido,
+        "email": voluntario.email,
+    })
+
+    return render(request, "voluntariado/formVol.html", {"form": mi_form})
+
+
 def nuevo_voluntario(request):
     if request.method == "POST":
         mi_form = FormVoluntario(request.POST)
@@ -85,6 +119,13 @@ def nuevo_voluntario(request):
     mi_form = FormVoluntario()
 
     return render(request, "voluntariado/formVol.html", {"form": mi_form})
+
+
+def eliminar_voluntario(request, id):
+    voluntario = Voluntario.objects.get(id=id)
+    voluntario.delete()
+
+    return redirect("Voluntarios")
 
 
 def buscar_voluntario(request, nombre):
